549c8bfa9eadc70fa4ac4f06ea2aae98098c9b9c
Piyush Swain <piyushswain@Piyushs-MBP.domain.name>
2022-03-04 12:34:11 +0530

    Modify checkout helper to use exisitng razorpay orders
    This commit modifies the checkout_helper to use an existing razorpay_order
    
    Earlier behaviour created a new razorpay_order for every payment request which was wrong.
    Now whenever a payment request is made, the spree_order is checked if it has an existing razorpay_order.
    If a razorpay_order exists, then payments are made against the existing order, else a new razorpay_order is created.

--- app/helpers/solidus_razorpay/api/checkout_helper.rb
+++ app/helpers/solidus_razorpay/api/checkout_helper.rb
@@ -4,15 +4,24 @@ module SolidusRazorpay
   module Api
     module CheckoutHelper
       def create_order(params, payment_method)
-        receipt = params[:receipt]
-        amount = params[:amount]
-        order = Spree::Order.find(params[:orderId])
-
         razorpay_key = payment_method.preferences[:razorpay_key]
         razorpay_secret = payment_method.preferences[:razorpay_secret]
-
         gateway = SolidusRazorpay::Gateway.new({ razorpay_key: razorpay_key, razorpay_secret: razorpay_secret })
-        gateway.create_order(amount, receipt, order.currency)
+
+        receipt = params[:receipt]
+        amount = params[:amount]
+
+        order = Spree::Order.find(params[:orderId])
+        razorpay_order_id = order.razorpay_order_id
+
+        razorpay_order = if razorpay_order_id.present?
+                           gateway.retrieve_order(razorpay_order_id)
+                         else
+                           gateway.create_order(amount, receipt, order.currency)
+                         end
+
+        order.update(razorpay_order_id: razorpay_order.id)
+        razorpay_order
       end
     end
   end
