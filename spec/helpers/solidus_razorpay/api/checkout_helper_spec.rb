829116677453f030eac63ffad304e4a6a716feba
Piyush Swain <piyushswain@Piyushs-MBP.domain.name>
2022-03-04 12:34:11 +0530

    Modify checkout helper to use exisitng razorpay orders
    This commit modifies the checkout_helper to use an existing razorpay_order
    
    Earlier behaviour created a new razorpay_order for every payment request which was wrong.
    Now whenever a payment request is made, the spree_order is checked if it has an existing razorpay_order.
    If a razorpay_order exists, then payments are made against the existing order, else a new razorpay_order is created.

--- spec/helpers/solidus_razorpay/api/checkout_helper_spec.rb
+++ spec/helpers/solidus_razorpay/api/checkout_helper_spec.rb
@@ -3,7 +3,23 @@ require 'spec_helper'
 RSpec.describe SolidusRazorpay::Api::CheckoutHelper, type: :helper do
   let(:spree_user) { create(:user_with_addresses) }
   let(:spree_address) { spree_user.addresses.first }
-  let(:order) { create(:order, bill_address: spree_address, ship_address: spree_address, user: spree_user) }
+  let(:order) {
+    create(
+      :order,
+      bill_address: spree_address,
+      ship_address: spree_address,
+      user: spree_user
+    )
+  }
+  let(:order_with_razorpay) {
+    create(
+      :order,
+      bill_address: spree_address,
+      ship_address: spree_address,
+      user: spree_user,
+      razorpay_order_id: 'order_IgpDqlOTp1beGM'
+    )
+  }
   let(:amount) { order.display_outstanding_balance.money.fractional }
   let(:payment_method) { create(:razorpay_payment_method) }
   let(:razorpay_order) {
@@ -26,16 +42,36 @@ RSpec.describe SolidusRazorpay::Api::CheckoutHelper, type: :helper do
   end
 
   describe '#create_order' do
-    subject(:response) { create_order(params, payment_method) }
+    context 'when order does not have a razorpay_order_id' do
+      subject(:response) { create_order(params, payment_method) }
 
-    let(:params) { { orderId: order.id, amount: amount, receipt: order.number } }
+      let(:params) { { orderId: order.id, amount: amount, receipt: order.number } }
 
-    it 'successfully returns a Razorpay::Order object' do
-      expect(response.class).to eq Razorpay::Order
+      it 'successfully returns a Razorpay::Order object' do
+        expect(response.class).to eq Razorpay::Order
+      end
+
+      it 'returns the correct order id in the object' do
+        expect(response.id).to eq 'order_IgpDqlOTp1beGM'
+      end
     end
 
-    it 'returns the correct order id in the object' do
-      expect(response.id).to eq 'order_IgpDqlOTp1beGM'
+    context 'when order has a razorpay_order_id' do
+      subject(:response) { create_order(params, payment_method) }
+
+      let(:params) { { orderId: order_with_razorpay.id, amount: amount, receipt: order_with_razorpay.number } }
+
+      before do
+        allow(Razorpay::Order).to receive(:fetch) { razorpay_order }
+      end
+
+      it 'successfully returns a Razorpay::Order object' do
+        expect(response.class).to eq Razorpay::Order
+      end
+
+      it 'returns the correct order id in the object' do
+        expect(response.id).to eq 'order_IgpDqlOTp1beGM'
+      end
     end
   end
 end
