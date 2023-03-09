027be874d0cec7beb27270aa8a89a74de280a0e1
piyushswain <piyush.swain3@gmail.com>
2023-03-09 15:33:14 +0530

    Configure static sources within to_prepare blocks
    This is needed in Rails 7 [1] and latest Solidus was adapted to support
    it. See solidusio/solidus#4449
    
    [1] - https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#autoload-on-boot-and-on-each-reload

--- lib/solidus_razorpay/engine.rb
+++ lib/solidus_razorpay/engine.rb
@@ -12,14 +12,17 @@ module SolidusRazorpay
     engine_name 'solidus_razorpay'
 
     initializer "solidus_razorpay.add_static_preference", after: "spree.register.payment_methods" do |app|
-      app.config.spree.payment_methods << SolidusRazorpay::RazorpayPayment
-      Spree::Config.static_model_preferences.add(
-        SolidusRazorpay::RazorpayPayment,
-        'razorpay_credentials', {
-          razorpay_key: ENV['RAZORPAY_KEY'],
-          razorpay_secret: ENV['RAZORPAY_SECRET']
-        }
-      )
+      app.config.spree.payment_methods << 'SolidusRazorpay::RazorpayPayment'
+
+      app.config.to_prepare do
+        Spree::Config.static_model_preferences.add(
+          SolidusRazorpay::RazorpayPayment,
+          'razorpay_credentials', {
+            razorpay_key: ENV['RAZORPAY_KEY'],
+            razorpay_secret: ENV['RAZORPAY_SECRET']
+          }
+        )
+      end
       Spree::PermittedAttributes.source_attributes.concat [
         :razorpay_order_id,
         :razorpay_payment_id,
